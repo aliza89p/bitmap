cdc3285369aa22dcbd86ce0bb1064655e3d915d5
Aliza Pilisuk <apilisuk@gmail.com>
2016-05-14 10:11:47 -0700

    messing around with transform

--- transform.js
+++ transform.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const file = fs.readFileSync('img/non-palette-bitmap.bmp');
+const file = fs.readFileSync('img/palette-bitmap.bmp');
 
 const headers = {};
 console.log(file.slice(0, 4));
@@ -8,9 +8,11 @@ console.log(file[0], file[1]);
 
 
 headers.type = file.toString('ascii', 0, 2);
+//
 headers.size = file.readUInt32LE(2);
 headers.pixelStart = file.readUInt32LE(10);
-
+headers.numOfColors = file.readUInt32LE(46);
+headers.numOfBits = file.readUInt32LE(28);
 console.log(file.readUInt32LE(54));
 
-// console.log(headers);
+console.log(headers);
