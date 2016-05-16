f298ebb7afd9924531d2c7a317bfae0ca3701abb
Aliza Pilisuk <apilisuk@gmail.com>
2016-05-16 11:21:15 -0700

    working on exporting headers

--- test/test.js
+++ test/test.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
 const expect = require('chai').expect;
 const script = require('../transform');
+const headers = headers;
 
 describe('Different images', () => {
   it('The first image shoudl be different than the second image', () => {
     expect(fs.readFileSync('img/palette-bitmap.bmp')).to.not.eql(fs.readFileSync('img/new-palette-bitmap.bmp'));
+  });
+});
+
+describe('headers exporting', () => {
+  before(done => {
+    fs.readFile('../transform.js', done);
+  });
+  it('should have the type BM', () => {
+    expect(headers).to.not.have.property('type', 'BM');
   });
 });
